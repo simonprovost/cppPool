1305e89e43bfd07ab900931efaebdf456fae1e10
SimonProvost <simon1.provost@epitech.eu>
2019-05-05 14:48:23 +0200

    feat(master): Add all days of CPP Pool. {Epitech}

---
+++ pull_all_day.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash 
+
+git clone git@git.epitech.eu:/simon1.provost@epitech.eu/cpp_d02m_2018
+git clone git@git.epitech.eu:/simon1.provost@epitech.eu/cpp_d14a_2018
+git clone git@git.epitech.eu:/simon1.provost@epitech.eu/cpp_d09_2018
+git clone git@git.epitech.eu:/simon1.provost@epitech.eu/cpp_d06_2018
+git clone git@git.epitech.eu:/simon1.provost@epitech.eu/cpp_d14m_2018
+git clone git@git.epitech.eu:/simon1.provost@epitech.eu/cpp_d10_2018
+git clone git@git.epitech.eu:/simon1.provost@epitech.eu/cpp_d02m
+git clone git@git.epitech.eu:/simon1.provost@epitech.eu/cpp_d13_2018
+git clone git@git.epitech.eu:/simon1.provost@epitech.eu/cpp_d15_2018
+git clone git@git.epitech.eu:/simon1.provost@epitech.eu/cpp_d03_2018
+git clone git@git.epitech.eu:/simon1.provost@epitech.eu/cpp_d01_2018
+git clone git@git.epitech.eu:/simon1.provost@epitech.eu/cpp_d02a_2018
+git clone git@git.epitech.eu:/simon1.provost@epitech.eu/cpp_d16_2018
+git clone git@git.epitech.eu:/simon1.provost@epitech.eu/cpp_d02a
+git clone git@git.epitech.eu:/simon1.provost@epitech.eu/cpp_d08_2018
+git clone git@git.epitech.eu:/simon1.provost@epitech.eu/cpp_d17_2018
+git clone git@git.epitech.eu:/simon1.provost@epitech.eu/cpp_d07a_2018
+git clone git@git.epitech.eu:/simon1.provost@epitech.eu/cpp_d07m_2018
---
+++ rm_all_git.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash 
+
+rm -rf cpp_d02m_2018/.git
+rm -rf cpp_d14a_2018/.git
+rm -rf cpp_d09_2018/.git
+rm -rf cpp_d06_2018/.git
+rm -rf cpp_d14m_2018/.git
+rm -rf cpp_d10_2018/.git
+rm -rf cpp_d02m/.git
+rm -rf cpp_d13_2018/.git
+rm -rf cpp_d15_2018/.git
+rm -rf cpp_d03_2018/.git
+rm -rf cpp_d01_2018/.git
+rm -rf cpp_d02a_2018/.git
+rm -rf cpp_d16_2018/.git
+rm -rf cpp_d02a/.git
+rm -rf cpp_d08_2018/.git
+rm -rf cpp_d17_2018/.git
+rm -rf cpp_d07a_2018/.git
+rm -rf cpp_d07m_2018/.git
