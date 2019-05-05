17ffba6572f4143bc3ce9909bef48c4976b8f958
SimonProvost <simon1.provost@epitech.eu>
2019-05-05 15:00:06 +0200

    feat(master): Add all days of CPP Pool. {Epitech}

--- rm_all_git.sh
+++ rm_all_git.sh
@@ -1,39 +1,39 @@
 #!/usr/bin/env bash 
 
-rm -rf cpp_d02m_2018/.git
-rm -rf cpp_d14a_2018/.git
-rm -rf cpp_d09_2018/.git
-rm -rf cpp_d06_2018/.git
-rm -rf cpp_d14m_2018/.git
-rm -rf cpp_d10_2018/.git
-rm -rf cpp_d02m/.git
-rm -rf cpp_d13_2018/.git
-rm -rf cpp_d15_2018/.git
-rm -rf cpp_d03_2018/.git
-rm -rf cpp_d01_2018/.git
-rm -rf cpp_d02a_2018/.git
-rm -rf cpp_d16_2018/.git
-rm -rf cpp_d02a/.git
-rm -rf cpp_d08_2018/.git
-rm -rf cpp_d17_2018/.git
-rm -rf cpp_d07a_2018/.git
-rm -rf cpp_d07m_2018/.git
+# rm -rf cpp_d02m_2018/.git
+# rm -rf cpp_d14a_2018/.git
+# rm -rf cpp_d09_2018/.git
+# rm -rf cpp_d06_2018/.git
+# rm -rf cpp_d14m_2018/.git
+# rm -rf cpp_d10_2018/.git
+# rm -rf cpp_d02m/.git
+# rm -rf cpp_d13_2018/.git
+# rm -rf cpp_d15_2018/.git
+# rm -rf cpp_d03_2018/.git
+# rm -rf cpp_d01_2018/.git
+# rm -rf cpp_d02a_2018/.git
+# rm -rf cpp_d16_2018/.git
+# rm -rf cpp_d02a/.git
+# rm -rf cpp_d08_2018/.git
+# rm -rf cpp_d17_2018/.git
+# rm -rf cpp_d07a_2018/.git
+# rm -rf cpp_d07m_2018/.git
 
-git rm --cached cpp_d02m_2018/.git
-git rm --cached cpp_d14a_2018/.git
-git rm --cached cpp_d09_2018/.git
-git rm --cached cpp_d06_2018/.git
-git rm --cached cpp_d14m_2018/.git
-git rm --cached cpp_d10_2018/.git
-git rm --cached cpp_d02m/.git
-git rm --cached cpp_d13_2018/.git
-git rm --cached cpp_d15_2018/.git
-git rm --cached cpp_d03_2018/.git
-git rm --cached cpp_d01_2018/.git
-git rm --cached cpp_d02a_2018/.git
-git rm --cached cpp_d16_2018/.git
-git rm --cached cpp_d02a/.git
-git rm --cached cpp_d08_2018/.git
-git rm --cached cpp_d17_2018/.git
-git rm --cached cpp_d07a_2018/.git
-git rm --cached cpp_d07m_2018/.git
+git rm --cached cpp_d02m_2018
+git rm --cached cpp_d14a_2018
+git rm --cached cpp_d09_2018
+git rm --cached cpp_d06_2018
+git rm --cached cpp_d14m_2018
+git rm --cached cpp_d10_2018
+git rm --cached cpp_d02m
+git rm --cached cpp_d13_2018
+git rm --cached cpp_d15_2018
+git rm --cached cpp_d03_2018
+git rm --cached cpp_d01_2018
+git rm --cached cpp_d02a_2018
+git rm --cached cpp_d16_2018
+git rm --cached cpp_d02a
+git rm --cached cpp_d08_2018
+git rm --cached cpp_d17_2018
+git rm --cached cpp_d07a_2018
+git rm --cached cpp_d07m_2018
